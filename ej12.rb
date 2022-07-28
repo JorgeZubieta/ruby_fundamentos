10d8e29b2b952cd9afd27e9ec4541acdfd08bc00
jorge zubieta <zubietajor@gmail.com>
2022-07-28 11:30:15 -0300

    ej12 primer programa en ruby efecto y objeto terminado

--- ej12.rb
+++ ej12.rb
@@ -1,25 +1,34 @@
-# comer_lombriz! hace que la energia de Pepita aumente en 20 unidades;
-# volar_en_circulos! hace que la energia de Pepita disminuya en 10 unidades.
+# Ya vimos que un objeto puede entender múltiples mensajes, y esos mensajes conforman su interfaz.
+# ¿Pero podría haber más de un objeto que entienda los mismos mensajes?
+# A Pepita ya la conocemos bien: canta, come, etc. Su amiga Norita, por otro lado, no aprendió nunca a decirnos su energía. Y Mercedes es una reconocida cantora.
+# Usando la consola, averiguá cuál es la interfaz de cada una de ellas, y completá el listado de mensajes que cada una entiende en el editor.
 
-# hacer que Pepita coma y vuele hasta quedarse con 150 unidades de energía. Acordate que Pepita arranca con la energía en 100.
+interfaz_pepita = %w(
+    energia
+    cantar!
+    comer_lombriz!
+    volar_en_circulos!
+)
+    
+interfaz_norita = %w(
+    cantar!
+    comer_lombriz!
+    volar_en_circulos!
+)
+    
+interfaz_mercedes = %w(
+    cantar!
+)
 
-# PROGRAMA
-Pepita.energia
-Pepita.volar_en_circulos!
-Pepita.comer_lombriz!
-Pepita.comer_lombriz!
-Pepita.comer_lombriz!
+#   ¡Así es! Puede haber más de un objeto que entienda el mismo mensaje. Notá que sin embargo no todos los objetos están obligados a reaccionar de igual forma ante el mismo mensaje:
 
-# COSNOLA
-> Pepita.energia
-=> 100
-> Pepita.volar_en_circulos!
-=> nil
-> Pepita.comer_lombriz!
-=> nil
-> Pepita.comer_lombriz!
-=> nil
-> Pepita.comer_lombriz!
-=> nil
-> Pepita.energia
-=> 150
+Pepita.cantar!
+=> "pri pri pri"
+
+Norita.cantar!
+=> "priiiip priiiip"
+
+Mercedes.cantar!
+=> "♪ una voz antigua de viento y de sal ♫"
+
+# Esto significa que dos o más objetos pueden entender un mismo mensaje, pero pueden comportarse de formas diferentes. Ya hablaremos más de esto en próximas lecciones.
