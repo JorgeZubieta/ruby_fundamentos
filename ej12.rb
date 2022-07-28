893d7164f184bfe9e35c115ca39ff15017c34279
jorge zubieta <zubietajor@gmail.com>
2022-07-28 11:07:28 -0300

    ej12 primer programa en ruby efecto y objeto

---
+++ ej12.rb
@@ -0,0 +1,25 @@
+# comer_lombriz! hace que la energia de Pepita aumente en 20 unidades;
+# volar_en_circulos! hace que la energia de Pepita disminuya en 10 unidades.
+
+# hacer que Pepita coma y vuele hasta quedarse con 150 unidades de energía. Acordate que Pepita arranca con la energía en 100.
+
+# PROGRAMA
+Pepita.energia
+Pepita.volar_en_circulos!
+Pepita.comer_lombriz!
+Pepita.comer_lombriz!
+Pepita.comer_lombriz!
+
+# COSNOLA
+> Pepita.energia
+=> 100
+> Pepita.volar_en_circulos!
+=> nil
+> Pepita.comer_lombriz!
+=> nil
+> Pepita.comer_lombriz!
+=> nil
+> Pepita.comer_lombriz!
+=> nil
+> Pepita.energia
+=> 150
