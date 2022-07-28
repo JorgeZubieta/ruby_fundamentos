aa3cc4b67b7d7f326358e3b95afc8d47ce977072
jorge zubieta <zubietajor@gmail.com>
2022-07-28 14:23:07 -0300

    carpetas ordenadas y creacion del ej01 de modulos

---
+++ 2. metodos_y_estados/ej01.rb
@@ -0,0 +1,24 @@
+# Ejercicio 1: Creando a Pepita
+# Inicialmente en el ambiente solo existen objetos simples como números, strings y booleanos.
+
+# Pero como es imposible que quienes diseñan un lenguaje puedan precargar objetos para solucionar todos nuestros problemas, también nos dan la posibilidad de crear los nuestros.
+
+# En Ruby, si quisiéramos definir a Norita, escribiríamos el siguiente código:
+
+module Norita
+
+end
+
+# Sí, así de simple.
+# ¿Te animás a modificar nuestro código para crear a Pepita?
+
+
+# Solución # Consola
+
+module Pepita
+
+end
+
+# ¡Muy bien! Tu solución pasó todas las pruebas
+# ¡Muy bien, Pepita vive!
+# Como dedujiste, la definición de un objeto se inicia con la palabra reservada module, luego el nombre del objeto (con la primera letra en mayúscula) y su fin se indica con un end.
