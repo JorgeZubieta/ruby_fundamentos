ff18177e2bc1140b813dca00a49ebee3eee57585
jorge zubieta <zubietajor@gmail.com>
2022-08-05 15:41:35 -0300

    ej01_variables_upcase

---
+++ 4. referencias/ej01_variables_y_upcase.rb
@@ -0,0 +1,18 @@
+# Este es un programa que convierte en mayúsculas al string "hola".
+
+> "hola".upcase
+=> "HOLA"
+
+# Sin embargo, podemos hacer algo más: declarar variables. Por ejemplo, podemos declarar una variable saludo, inicializarla con "hola", enviarle mensajes...
+
+> saludo = "hola"
+> saludo.upcase
+=> "HOLA"
+
+# ...y esperar el mismo resultado que para el programa anterior.
+
+# Veamos si queda claro: agregá al programa anterior una variable saludo_formal, inicializada con "buen día"
+
+saludo = "hola"
+#Escribí acá...
+saludo_formal = "buen día"
