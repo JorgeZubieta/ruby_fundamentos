2ae69db6aea54cc3e065a4276efae7fff63913c3
jorge zubieta <zubietajor@gmail.com>
2022-08-10 19:31:39 -0300

    ej02 notebook terminado

--- 7. herencia/ej02_notebook.rb
+++ 7. herencia/ej02_notebook.rb
@@ -15,3 +15,7 @@ class Notebook
         @bateria = 100
     end     
 end
+
+# ¡Muy bien! Pero... las clases Celular y Notebook son demasiado parecidas, ¿no?
+# Más específicamente en los métodos initialize y cargar_a_tope! son iguales.
+# ¡Obviamente se puede evitar esa repetición de lógica! Vamos al siguiente ejercicio a ver cómo.
