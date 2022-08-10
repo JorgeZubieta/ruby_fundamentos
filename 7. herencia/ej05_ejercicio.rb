29fe05406282bfb8be4d3a15a8bb19a70236fa43
jorge zubieta <zubietajor@gmail.com>
2022-08-10 20:55:32 -0300

    ej05 terminado + nota

--- 7. herencia/ej05_ejercicio.rb
+++ 7. herencia/ej05_ejercicio.rb
@@ -27,3 +27,13 @@ class Notebook < Dispositivo
         @bateria = bateria - minutos
     end
 end
+
+# ¿Acaso para la Notebook no deberíamos preguntar si está descargada? en lugar de descargado??
+
+# ¡Puede ser! Pero si tuvieramos un método por cada clase, no podríamos tratar polimórficamente a los objetos. Por ejemplo, no habría manera de saber cuántos dispositivos están descargados porque no existiría un único mensaje que respondiera nuestra pregunta.
+
+> dispositivos.count { |dispositivo| dispositivo.descargado? }
+=> #¡Fallaría porque Notebook no podría responder al mensaje descargado?!
+
+> dispositivos.count { |dispositivo| dispositivo.descargada? }
+=> #¡Fallaría porque Celular no podría responder al mensaje descargada?!
